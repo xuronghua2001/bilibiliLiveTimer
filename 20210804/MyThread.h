135c67982943338d3fdca23ac884d49de84df227
xuronghua2001 <xuronghua2001@163.com>
2022-01-18 03:01:46 +0800

    Create MyThread.h

---
+++ 20210804/MyThread.h
@@ -0,0 +1,21 @@
+#ifndef MYTHREAD_H
+#define MYTHREAD_H
+#include <QObject>
+class MyThread : public QObject
+{
+  Q_OBJECT
+      public:
+  int id=0;
+  explicit MyThread(QObject *parent = nullptr);
+
+  void setFlag(bool flag = true);
+signals:
+
+  void mySignal();
+
+public slots:  void myTimeout();
+private:
+  bool isStop;
+};
+
+#endif // MYTHREAD_H
