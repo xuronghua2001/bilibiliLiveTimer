0e7798c7896883ecbfa854c98ca119aa852c8506
xuronghua2001 <xuronghua2001@163.com>
2022-02-07 02:55:55 +0800

    Update MyThread.h

--- 20210804/MyThread.h
+++ 20210804/MyThread.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef MYTHREAD_H
 #define MYTHREAD_H
 #include <QObject>
@@ -19,3 +20,26 @@ private:
 };
 
 #endif // MYTHREAD_H
+=======
+#ifndef MYTHREAD_H
+#define MYTHREAD_H
+#include <QObject>
+class MyThread : public QObject
+{
+  Q_OBJECT
+      public:
+  int id=0;
+  explicit MyThread(QObject *parent = nullptr);
+
+  void setFlag(bool flag = true);
+signals:
+
+  void mySignal();
+
+public slots:  void myTimeout();
+private:
+  bool isStop;
+};
+
+#endif // MYTHREAD_H
+>>>>>>> Stashed changes
