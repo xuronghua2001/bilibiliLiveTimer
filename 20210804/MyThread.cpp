ceca5ded7a665408cc87a168ee0fc3a04fdbb539
xuronghua2001 <xuronghua2001@163.com>
2022-02-07 02:55:53 +0800

    Update MyThread.cpp

--- 20210804/MyThread.cpp
+++ 20210804/MyThread.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include "MyThread.h"
 #include <QThread>
 #include<QDebug>
@@ -20,3 +21,27 @@ void MyThread::setFlag(bool flag)
 {
   isStop = flag;//定义标志位用于判断线程是否终止
 }
+=======
+#include "MyThread.h"
+#include <QThread>
+#include<QDebug>
+MyThread::MyThread(QObject *parent) : QObject(parent)
+{
+  isStop = false;
+}
+
+void MyThread::myTimeout()
+{//qDebug()<<"开启多线程";
+  while(isStop == false)
+  {
+    id++;
+    QThread::sleep(3);//强制暂停3秒，使得每三秒产生一次信号，
+    emit mySignal();//每3秒发送一次信号给mainwindow.cpp
+    //qDebug()<<"子线程号"<<QThread::currentThread();
+  }
+}
+void MyThread::setFlag(bool flag)
+{
+  isStop = flag;//定义标志位用于判断线程是否终止
+}
+>>>>>>> Stashed changes
