0eefa62eed8e0311f8e3134fba126f9e9f82cfbc
xuronghua2001 <xuronghua2001@163.com>
2022-02-08 20:06:25 +0800

    Update main.cpp

--- 20210804/main.cpp
+++ 20210804/main.cpp
@@ -5,11 +5,12 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
- /* QFile file(":/Ubuntu.qss");
+  QFile file(":/myqss.qss");
   file.open(QFile::ReadOnly);
   QTextStream filetext(&file);
   QString styleSheet = filetext.readAll();
-  qApp->setStyleSheet(styleSheet);*/
+  qApp->setStyleSheet(styleSheet);
+  file.close();
     Widget w;
     w.show();
     return a.exec();
